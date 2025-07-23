589fca47da0975d5d7b0cb1156570bcb4701130f
Sawacha Emmanuel <sawachaemmanuel@gmail.com>
2025-07-23 12:01:49 +0100

    supabase changes

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://vhtsmzabffhwufmteurt.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZodHNtemFiZmZod3VmbXRldXJ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI5MTM1OTIsImV4cCI6MjA2ODQ4OTU5Mn0.Q9SPAcQE_qoQ-NVWtfQ8B3n7pwG7SseprpOpYHzamF0';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
